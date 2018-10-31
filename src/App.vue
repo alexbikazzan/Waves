b07cf94f2b49a701b98b5541013e2607d200c806
Alex Bikazzan <alex.bikazzan@gmail.com>
2018-10-31 23:58:16 +0100

    Button styling part 1

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,13 @@
                  :key="color.id"
                  :color="color" />
     </div>
+    <div class="button-wrapper">
+      <button :disabled="showMoreButton"
+              class="button"
+              @click="nextPage">
+        More colors
+      </button>
+    </div>
   </div>
 </template>
 
@@ -19,17 +26,27 @@ export default {
     ColorBox,
   },
   data: () => ({
-    pageCount: 1,
+    totalPageCount: 1,
+    currentPage: 1,
     colors: [],
   }),
+  computed: {
+    showMoreButton() {
+      return this.currentPage >= this.totalPageCount;
+    },
+  },
   created() {
     this.getColors();
   },
   methods: {
-    async getColors() {
-      const result = await api.colors();
+    async getColors(page = 1) {
+      const result = await api.colors(page);
       this.colors = result.data;
-      this.pageCount = result.total_pages;
+      this.page = result.page;
+      this.totalPageCount = result.total_pages;
+    },
+    nextPage() {
+      this.getColors((this.currentPage += 1));
     },
   },
 };
@@ -38,8 +55,6 @@ export default {
 <style lang="scss">
 :root {
   --spacing-default: 1.2rem;
-  --breakpoint-tablet: 768px;
-  --breakpoint-desktop: 1024px;
 }
 
 * {
@@ -68,11 +83,12 @@ html {
 
 body {
   font-family: 'Fjalla One', sans-serif;
-  background: #3b354a;
+  background: #f6f6f6;
 }
 
 .app-wrapper {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   padding: var(--spacing-default);
@@ -84,7 +100,7 @@ body {
   display: grid;
   grid-template-columns: repeat(1, 1fr);
   max-width: 720px;
-  margin: auto;
+  margin: 0 auto;
   grid-gap: var(--spacing-default);
   @media (min-width: 480px) {
     grid-template-columns: repeat(2, 1fr);
@@ -92,5 +108,36 @@ body {
   @media (min-width: 720px) {
     grid-template-columns: repeat(4, 1fr);
   }
+}
+
+.button-wrapper {
+  margin-top: calc(var(--spacing-default) * 2);
+  display: flex;
+}
+
+.button {
+  position: relative;
+  border: none;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
+  zoom: 1;
+  overflow: visible;
+  text-decoration: none;
+  text-align: center;
+  border-radius: 4px;
+  height: 50px;
+  line-height: 50px;
+  padding: 0 2.2em;
+  white-space: nowrap;
+  touch-action: manipulation;
+  transition: all 0.4s;
+  font-size: 1.18rem;
+  outline: none;
+  border-radius: 25px;
+  padding: 0 2.2em;
+  background: hsla(228, 30%, 70%, 1);
+  color: hsl(228, 26%, 20%);
 }
 </style>
