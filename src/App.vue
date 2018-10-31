0caa610efe2bba7907179e98ee65df5037d5e89f
Alex Bikazzan <alex.bikazzan@gmail.com>
2018-10-31 22:09:59 +0100

    Basic styles and boxes grid

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,87 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app"
+       class="app-wrapper">
+    <div class="colors-wrapper">
+      <color-box v-for="color in colors"
+                 :key="color.id"
+                 :color="color" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import api from './services/ApiService';
+import ColorBox from './components/ColorBox';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    ColorBox,
+  },
+  data: () => ({
+    pageCount: 1,
+    colors: [],
+  }),
+  created() {
+    this.getColors();
+  },
+  methods: {
+    async getColors() {
+      const result = await api.colors();
+      this.colors = result.data;
+      this.pageCount = result.total_pages;
+    },
   },
 };
 </script>
 
 <style lang="scss">
+:root {
+  --default-spacing: 1.2rem;
+}
+
+* {
+  box-sizing: border-box;
+  -webkit-overflow-scrolling: touch;
+}
+
+html,
+body {
+  -webkit-font-smoothing: antialiased;
+  position: relative;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+html {
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  font-size: 100%;
+  -ms-overflow-style: -ms-autohiding-scrollbar;
+  /* Edge 12+, Internet Explorer 11- */
+  touch-action: manipulation;
+}
+
+body {
+  font-family: 'Fjalla One', sans-serif;
+}
+
+.app-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: var(--default-spacing);
+  height: 100%;
+  width: 100%;
+}
+
+.colors-wrapper {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  max-width: 720px;
+  margin: auto;
+  grid-gap: var(--default-spacing);
+}
 </style>
---
+++ src/components/ColorBox.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="color-box"
+       :style="styleObject">
+    {{ color.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    color: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    styleObject() {
+      return {
+        background: this.color.color,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.color-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  color: #000;
+  height: 200px;
+  width: 200px;
+  font-size: 1.2em;
+  font-weight: 700;
+}
+</style>
